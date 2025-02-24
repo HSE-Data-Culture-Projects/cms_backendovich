32313bce60469f84ee90aa88f460c5d48b0cc97a
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2025-02-24 16:06:48 +0300

    Multifile loader

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -18,6 +18,7 @@ router.get('/topic/:topicId', taskController.getTasksByTopicId);
 router.patch('/:id', authorize(['admin']), upload.single('file'), taskController.updateTask);
 router.delete('/:id', authorize(['admin']), taskController.deleteTask);
 router.post('/import', authorize(['admin']), upload.single('file'), taskController.importTasks);
-router.post('/import-multiple', authorize(['admin']), upload.array('files', 10), taskController.importMultipleTasks);
+router.post('/import-xml', authorize(['admin']), upload.array('files', 10), taskController.importXmlQuestions);
+
 
 module.exports = router;
