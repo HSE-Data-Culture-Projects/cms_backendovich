844361de1497650533fa4aeb051a916e0b50a898
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2025-02-24 16:28:20 +0300

    Multifile loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -8,10 +7,24 @@ const logger = require('./utils/logger');
 
 const app = express();
 
-app.use(cors());
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
+app.use((req, res, next) => {
+  const contentType = req.headers['content-type'] || '';
+  if (contentType.includes('multipart/form-data')) {
+    return next();
+  }
+  express.json({ limit: '100mb' })(req, res, next);
+});
+
+app.use((req, res, next) => {
+  const contentType = req.headers['content-type'] || '';
+  if (contentType.includes('multipart/form-data')) {
+    return next();
+  }
+  express.urlencoded({ limit: '100mb', extended: true })(req, res, next);
+});
+
+app.use(cors());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', require('./routes/authRoutes'));
@@ -22,12 +35,12 @@ app.use('/api/gigachat', require('./routes/gigaChatRoutes'));
 
 const PORT = process.env.PORT || 3000;
 db.sequelize
-    .sync({ force: false })
-    .then(() => {
-        app.listen(PORT, () => {
-            logger.info(`Server is running on port ${PORT}.`);
-        });
-    })
-    .catch((error) => {
-        logger.error('Error syncing database:', error);
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      logger.info(`Server is running on port ${PORT}.`);
     });
+  })
+  .catch((error) => {
+    logger.error('Error syncing database:', error);
+  });
